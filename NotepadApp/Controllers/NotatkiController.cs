5193776da701f3e2b689edb4216ad2001da1b6b6
MartinKuzdowicz <martinkuzdowicz@gamil.com>
2016-04-19 17:22:26 +0200

    notatka form

---
+++ NotepadApp/Controllers/NotatkiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotepadApp.Controllers
+{
+    public class NotatkiController : Controller
+    {
+        // GET: Notatki
+        public ActionResult AddNew()
+        {
+            return View("NotatkiPage");
+        }
+    }
+}
---
+++ NotepadApp/Models/NoteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotepadApp.Models
+{
+    public class NoteModel
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+    }
+}
